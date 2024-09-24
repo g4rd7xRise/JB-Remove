611c1e521e65e205f173a5699194f0572f30b776
Rise <51847254+g4rd7xRise@users.noreply.github.com>
2024-09-24 22:51:56 +0300

    Update JBRemover.cmd

--- JBRemover.cmd
+++ JBRemover.cmd
@@ -1,8 +1,8 @@
 @echo off
-chcp 65001 >nul 2>&1  
+chcp 65001 >nul 2>&1
 setlocal
 
-set "success=0"  REM Flag to track success
+set "success=0"  
 
 set "userProfile=%USERPROFILE%"
 set "jetBrainsPath=%userProfile%\AppData\Roaming\JetBrains"
@@ -16,7 +16,9 @@ if exist "%jetBrainsPath%" (
     )
 ) else (
     echo JetBrains folder not found. Skipping deletion.
+    echo JetBrains is not installed.
 )
+
 
 reg delete "HKEY_CURRENT_USER\Software\JavaSoft\Prefs\jetbrains" /f
 if %ERRORLEVEL%==0 (
@@ -32,13 +34,17 @@ if %ERRORLEVEL%==0 (
     echo Error: Failed to delete registry keys for JetBrains. Error code: %ERRORLEVEL%.
 )
 
+
 if %success%==1 (
     echo Operation completed successfully.
 ) else (
     echo All operations failed.
+    start https://306.antroot.ru/jetbrains-activation  REM Redirect to the specified URL
 )
 
 endlocal
-pause  
+pause
 
-start https://github.com/g4rd7xRise/JB-Remove
+if %success%==1 (
+    start https://github.com/g4rd7xRise/JB-Remove
+)
